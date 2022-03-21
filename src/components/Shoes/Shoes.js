5e16faf07993d0f385d334418e0245a953da2fa1
zilurahman10 <raihan.brac2019@gmail.com>
2022-03-21 10:17:55 +0600

    third-commit

--- src/components/Shoes/Shoes.js
+++ src/components/Shoes/Shoes.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { multiply } from '../../Utilitis/calculate';
+import { add, multiply } from '../../Utilitis/calculate';
 
 const Shoes = () => {
     const num1 = 146;
     const num2 = 855;
     const result = multiply(num1, num2)
+    const sum = add(num1, num2)
     return (
         <div>
             <h1>This is shoe compo</h1>
             <h3>result : {result}</h3>
+            <h3>sum : {sum}</h3>
         </div>
     );
 };
