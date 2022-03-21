d77a68bf3e92e56baf2c3e6bcc12fa325a12245f
zilurahman10 <raihan.brac2019@gmail.com>
2022-03-21 09:34:50 +0600

    first-commit

---
+++ src/components/Cosmetics/Cosmetics.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cosmetics = () => {
+    return (
+        <div>
+            <h1>Welcome to my cosmetics store</h1>
+        </div>
+    );
+};
+
+export default Cosmetics;
